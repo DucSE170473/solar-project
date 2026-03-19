704f52faeb50096fe4142db24111eab1627c2c84
DucSE170473 <ducha.bidv@gmail.com>
2026-03-19 16:06:02 +0700

    fix: update destination path in vercel.json and refactor vite.config.ts for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.app.json'] }),
+    TanStackRouterVite(),
+    react(),
     tailwindcss(),
-    tanstackStart(),
-    viteReact(),
+    tsconfigPaths(),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
-
-export default config
